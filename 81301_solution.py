fe973bf86cd7732c3db8479939f3fb1dcf9b6b33
Jinwoo Shin <66043707+wkuwoo@users.noreply.github.com>
2021-10-07 13:18:58 +0900

    2021-10-07

--- 81301_solution.py
+++ 81301_solution.py
@@ -1,12 +1,13 @@
 # 숫자 문자열과 영어
 # https://programmers.co.kr/learn/courses/30/lessons/81301
+
 def solution(s):
     answer = ''
     dict = {}
-    info = ['zero', 'one', 'two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i in range(10):
+    info = ['zero', 'one', 'two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine'] # 0~9까지 영어 
+    for i in range(10): # info에 저장되어 있는 영어에 숫자를 부여하여, dict에 저장
         dict[info[i]] = i
-    print(dict)
+        print(dict)
     eng=''
     
     for i in s:
