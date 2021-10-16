48ba425215079c8d561d967396b01e20da712b72
Jinwoo Shin <66043707+wkuwoo@users.noreply.github.com>
2021-10-16 14:35:10 +0900

    Create 42889_solution.py

---
+++ Level_1/42889_solution.py
@@ -0,0 +1,14 @@
+def solution(N, stages):
+    fail = {}
+    users = len(stages)
+
+    for stage in range(1, N+1):
+        if users != 0:
+            fail_user = stages.count(stage)
+            print("1.",fail_user)
+            fail[stage] = fail_user / users 
+            print("2.",fail[stage])
+            users -= fail_user
+            print("3.",users)
+        else: fail[stage] = 0
+    return sorted(fail, key=lambda x: fail[x], reverse=True)
